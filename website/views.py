6e19c63647c0c99e7d96aefe97835899076633bc
Ricardo <ricardo.vaca@udla.edu.ec>
2024-12-26 21:39:00 -0500

    Configuración de la Vista principal con su ruta para los diferentes cálculos SQL para las tablas en el HTML

--- website/views.py
+++ website/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from .models import Employees, Departments, Spent
 
-# Function to define 
 def home_panel(request):
-    return render(request, 'homepanel.html', {})
+    
+    start_date = request.GET.get('start_date')  
+    end_date = request.GET.get('end_date')     
+
+    
+    employees = Employees.objects.all()
+    departments = Departments.objects.all()
+    spent = Spent.objects.all()
+
+   
+    if start_date and end_date:
+        department_expenses = Spent.objects.filter(
+            date_spent__range=[start_date, end_date]
+        ).values(
+            'department__name_department'
+        ).annotate(
+            total_spent=Sum('amount_spent')
+        ).order_by('-total_spent')
+    else:
+        
+        department_expenses = []
+
+    return render(request, 'homepanel.html', {
+        'employees': employees,
+        'departments': departments,
+        'spent': spent,
+        'department_expenses': department_expenses,
+        'start_date': start_date,
+        'end_date': end_date,
+    })
